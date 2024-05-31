01160fe7e3d6b786c52efcc4459b0028f5ba1e8f
Your Name <hyunyoo5064@naver.com>
2024-05-31 17:56:21 +0900

    포털 이미지 적용

--- hyunyoolim/portal.py
+++ hyunyoolim/portal.py
@@ -1,16 +1,15 @@
-# portal.py
-
 import pygame
 from setting import *
 
 class Portal:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = 700
+        self.y = 150
         self.width = 50
         self.height = 50
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
-
+        self.image = pygame.image.load('portal.png').convert_alpha()  # 이미지 불러오기
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))  # 이미지 크기 조절
+    
     def draw(self, screen):
-        pygame.draw.rect(screen, (0, 255, 0), self.rect)
-
+        screen.blit(self.image, (self.x, self.y))  # 이미지 그리기
