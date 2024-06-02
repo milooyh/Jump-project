6b57537b11ad652b98ea26ff35670ba43dfff145
Your Name <hyunyoo5064@naver.com>
2024-06-02 15:37:41 +0900

    아이템 클래스 추가 설정

--- hyunyoolim/item.py
+++ hyunyoolim/item.py
@@ -1,12 +1,30 @@
 import pygame
+import os
+from setting import *
 
 class Item:
-    def __init__(self, x, y, width, height, item_type, color):
+    def __init__(self, x, y, image_path):
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.item_type = item_type
-        self.color = color
-        self.rect = pygame.Rect(x, y, width, height)
+        self.width = 50
+        self.height = 50
+        base_path = os.path.dirname(os.path.realpath(__file__))
+        image_full_path = os.path.join(base_path, image_path)
+        self.image = pygame.image.load(image_full_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
+class HeartItem(Item):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'heart.png')
+
+class SpeedItem(Item):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'slow.png')
+
+class InvincibilityItem(Item):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'star.png')
