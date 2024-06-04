0204798779574f344887729769fafcf9a6015166
Your Name <hyunyoo5064@naver.com>
2024-06-04 23:00:21 +0900

    다음스테이지 이동 함수 정의 추가

--- hyunyoolim/screen.py
+++ hyunyoolim/screen.py
@@ -42,6 +42,7 @@ class Screen:
         screen.blit(text, text_rect)
         pygame.display.update()
         pygame.time.wait(2000)
+        game_manager.advance_stage()
 
     @staticmethod
     def show_game_over_screen(screen, game_manager):
