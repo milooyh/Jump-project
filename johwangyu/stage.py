064f442625f502e33037351fbdf1de5d249286c1
johwangyu <165298876+hwangyujo@users.noreply.github.com>
2024-06-07 05:25:22 +0900

    스테이지

--- johwangyu/stage.py
+++ johwangyu/stage.py
@@ -11,24 +11,6 @@ class Enemy:
         self.y = y
         self.direction = 1
 
-class ChasingEnemy:
-    def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
-
-    def update(self, player_x, player_y):
-        if player_x > self.x:
-            self.x += self.speed
-        elif player_x < self.x:
-            self.x -= self.speed
-
-    def draw(self, screen):
-        pygame.draw.polygon(screen, (0, 0, 0), [(self.x + 35//2, self.y),
-                                                (self.x + 35, self.y + 35//2),
-                                                (self.x + 35//2, self.y + 35),
-                                                (self.x, self.y + 35//2)])
-
 class PowerUp:
     def __init__(self, x, y):
         self.x = x
@@ -42,21 +24,26 @@ class Portal:
     def draw(self, screen):
         pygame.draw.ellipse(screen, (128, 0, 128), (self.x, self.y, 30, 80))
 
-def init_stage(blocks_positions, enemy_positions, powerup_positions):
+def init_stage(blocks_positions, enemy_positions, powerup_positions, portal_position):
     blocks = [Block(x, y) for x, y in blocks_positions]
     enemies = [Enemy(x, y) for x, y in enemy_positions]
     powerups = [PowerUp(x, y) for x, y in powerup_positions]
-    chasing_enemy = ChasingEnemy(650, 150, 2)
-    return blocks, enemies, powerups, chasing_enemy
+    portal = Portal(*portal_position)
+    return blocks, enemies, powerups, portal
 
 stages = {
-    1: ([(100, 500), (300, 400), (500, 300), (700, 200)],
-        [(150, 450), (350, 350), (550, 250), (750, 150)],
-        [(100 + (100 // 2 - 10), 500 - 20), (300 + (100 // 2 - 10), 400 - 20), (500 + (100 // 2 - 10), 300 - 20)]),
-    2: ([(150, 450), (350, 350), (500, 200), (700, 150), (200, 250), (600, 300)],
-        [(100, 450), (250, 350), (400, 250), (600, 150), (200, 400), (550, 300)],
-        [(150 + (100 // 2 - 10), 450 - 20), (350 + (100 // 2 - 10), 350 - 20), (500 + (100 // 2 - 10), 200 - 20)]),
-    3: ([(100, 450), (250, 350), (400, 250), (600, 150), (550, 300)],
-        [(100, 400), (250, 300), (400, 200), (650, 100), (600, 250)],
-        [(100 + (100 // 2 - 10), 450 - 20), (250 + (100 // 2 - 10), 350 - 20), (400 + (100 // 2 - 10), 250 - 20)])
+    1: ([(180, 350), (500, 350), (130, 200), (255, 270)],  # 파란색 발판
+        [(600, 315), (200, 165)],  # 초록색 적
+        [(240, 315), (170, 165)],  # 노란색 코인
+         (760, 365)),  # 포탈 위치 추가
+####################################################################################################
+    2: ([(170, 350), (80, 200), (50, 275), (375, 300), (420, 210), (550, 130), (760, 350)],
+        [(5, 315), (300, 165), (590, 95)],
+        [(120, 165), (590, 95)],
+        (762, 270)),
+####################################################################################################
+    3: ([(250, 350), (400, 250), (600, 150), (550, 300)],
+        [(100, 315),(250, 265), (400, 115), (600, 250)],
+        [(290, 315), (590, 265), (640, 115)],
+        (760, 365))
 }
