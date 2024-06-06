d2d13b7eb33cf34fec39233dd5a2d5ff5056270f
johwangyu <165298876+hwangyujo@users.noreply.github.com>
2024-06-07 05:30:00 +0900

    사용안함

--- testjohwangyu/stage3.py
+++ testjohwangyu/stage3.py
@@ -1,14 +1,4 @@
-from portal import Portal
-
-class Stage2:
-    def __init__(self):
-        self.blocks_positions = [
-            (100, 550),
-            (300, 450),
-            (500, 350),
-            (700, 250)
-        ]
-        self.portal = Portal(SCREEN_WIDTH - 50, SCREEN_HEIGHT - 150)import pygame
+import pygame
 import sys
 
 def show_game_over_screen(screen, score):
