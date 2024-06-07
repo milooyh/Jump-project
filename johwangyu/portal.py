367ecdf9d785b0b71fdc978d1721e9786b4158f2
KyoKwan <ahnkk0904@naver.com>
2024-06-08 00:08:43 +0900

    HARD

--- johwangyu/portal.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Portal:
-    def __init__(self, x, y):
-        self.x = x - 30
-        self.y = y
-
-    def draw(self, screen):
-        pygame.draw.ellipse(screen, (128, 0, 128), (self.x, self.y, 30, 80))
--- parksangyoung/obstacle.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-
-class Spike:
-    def __init__(self, x, y):
-        width, height = 20, 20
-        self.rect = pygame.Rect(x, y, width, height)
