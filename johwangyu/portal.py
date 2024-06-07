f1df5f5f7cef5a0838da7b33a289afb39d5c7010
KyoKwan <ahnkk0904@naver.com>
2024-06-08 02:02:23 +0900

    cancel

--- johwangyu/portal.py
+++ johwangyu/portal.py
@@ -1,21 +1,9 @@
 import pygame
-import os
 
 class Portal:
-    def __init__(self, x, y, target_stage):
+    def __init__(self, x, y):
         self.x = x - 30
         self.y = y
-        self.target_stage = target_stage
 
     def draw(self, screen):
         pygame.draw.ellipse(screen, (128, 0, 128), (self.x, self.y, 30, 80))
-
-    def check_collision(self, player_rect):
-        portal_rect = pygame.Rect(self.x, self.y, 30, 80)
-        return player_rect.colliderect(portal_rect)
-
-    def load_target_stage(self):
-        if self.target_stage == 1:
-            os.system("python /path/to/johwangyu/main_game.py")
-        elif self.target_stage == 2:
-            os.system("python /path/to/kyokwan/main_game.py")
