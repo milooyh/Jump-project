c7baa95bc4d975a3541b627734a9fdd1a0522fd9
psy0524 <117989384+psy0524@users.noreply.github.com>
2024-06-09 01:44:59 +0900

    주석문 삭제

--- KyoKwan/main_game.py
+++ KyoKwan/main_game.py
@@ -426,13 +426,6 @@ while running:
         for spike in spike_positions:
             pygame.draw.rect(screen, SPIKE_COLOR, (spike[0] - camera_x, spike[1], spike_width, spike_height))
 
-        # pygame.draw.rect(screen, (0, 255, 0), trigger_falling_block_zone.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (0, 0, 0), del_block_1.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (0, 255, 0), add_block_1.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (0, 0, 255), trigger_moving_block_zone.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (0, 255, 0), trigger_zone.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (0, 0, 255), spike_trigger_zone.move(-camera_x, 0), 2)
-        # pygame.draw.rect(screen, (255, 0, 0), teleport_zone, 2)
 
         portal_angle += 2
         rotated_portal_image = pygame.transform.rotate(portal_image, portal_angle)
