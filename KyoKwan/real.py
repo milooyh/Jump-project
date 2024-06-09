4d71726b16852f645e76b21e8ce352622cdf5881
KyoKwan <ahnkk0904@naver.com>
2024-06-10 05:42:16 +0900

    ###

--- KyoKwan/real.py
+++ KyoKwan/real.py
@@ -9,7 +9,10 @@ file_path = 'C:\\OSSW4\\your_file.txt'
 repo_path = 'C:\\OSSW4'
 
 # 커밋 메시지 설정
-commit_message = '#'
+commit_message = 'w'
+
+# 작업할 브랜치 설정
+branch_name = 'KyoKwan'
 
 while True:
     # 파일이 존재하지 않으면 생성
@@ -21,9 +24,16 @@ while True:
         f.write('#\n')
     
     # git 명령어 실행
-    subprocess.run(['git', 'add', file_path], cwd=repo_path)
-    subprocess.run(['git', 'commit', '-m', commit_message], cwd=repo_path)
-    subprocess.run(['git', 'push'], cwd=repo_path)
+    try:
+        # 원하는 브랜치로 체크아웃
+        subprocess.run(['git', 'checkout', branch_name], cwd=repo_path, check=True)
+
+        # 파일 추가, 커밋, 푸시
+        subprocess.run(['git', 'add', file_path], cwd=repo_path, check=True)
+        subprocess.run(['git', 'commit', '-m', commit_message], cwd=repo_path, check=True)
+        subprocess.run(['git', 'push', 'origin', branch_name], cwd=repo_path, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
     
     # 30초 대기
     time.sleep(30)
