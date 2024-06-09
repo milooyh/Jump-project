930dc0a17b0712f2e012720366423329d6c54377
Your Name <hyunyoo5064@naver.com>
2024-06-09 21:16:33 +0900

    폰트 설정

--- hyunyoolim/screen.py
+++ hyunyoolim/screen.py
@@ -16,7 +16,7 @@ class Screen:
     @staticmethod
     def show_start_screen(screen):
         screen.fill(WHITE) # 화면 흰색으로 채움
-        font = pygame.font.Font(None, 64)
+        font = pygame.font.Font(None, 64) # 폰트 설정
         text = font.render("Press SPACE to Start", True, BLACK)
         text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
         screen.blit(text, text_rect)
