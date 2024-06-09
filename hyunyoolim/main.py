61291c6b332bcf0f1cefbf5a819cba338547c526
Your Name <hyunyoo5064@naver.com>
2024-06-09 20:53:00 +0900

    게임 실행 main 함수

--- hyunyoolim/main.py
+++ hyunyoolim/main.py
@@ -4,6 +4,7 @@ from setting import * # 설정 파일에서 모든 설정 가져오기
 from game_manager import GameManager # 게임 관리자 모듈 임포트
 from screen import Screen # 화면 관련 모듈 임포트
 
+# 게임 실행 main 함수
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
