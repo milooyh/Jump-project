f75e6b5920f2b103ca17269c85d641308cc8390e
Your Name <hyunyoo5064@naver.com>
2024-06-09 18:41:14 +0900

    character_height: 캐릭터의 높이

--- hyunyoolim/block.py
+++ hyunyoolim/block.py
@@ -20,6 +20,8 @@ class Block:
     # character_x: 캐릭터의 x 좌표
     #  character_y: 캐릭터의 y 좌표
     # character_width: 캐릭터의 너비
+    # character_height: 캐릭터의 높이
+
     @staticmethod
     def check_collision(character_x, character_y, character_width, character_height, blocks):  
         for block in blocks:
