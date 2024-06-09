5d6e758040340001126a979b109d57377bef02b0
Your Name <hyunyoo5064@naver.com>
2024-06-09 20:03:37 +0900

    아이템 관련 모든 설정 가져오기

--- hyunyoolim/game_manager.py
+++ hyunyoolim/game_manager.py
@@ -8,7 +8,7 @@ from screen import Screen # 화면 클래스 임포트
 from block import Block # 발판 클래스 임포트
 from obstacle import Obstacle # 장애물 클래스 임포트
 from portal import Portal # 포털 클래스 임포트
-from item import *
+from item import * # 아이템 관련 모든 설정 가져오기
 
 class GameManager:
     
